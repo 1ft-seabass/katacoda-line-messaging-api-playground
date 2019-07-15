d01827eda2d2b613f8d12f6a7b65a8a14b175e8a
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-07-15 23:31:37 +0900

    upload

---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: 'channelSecret',
+    channelAccessToken: 'channelAccessToken'
+};
+
+// 日本語
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)'));
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+
+    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+        res.send('Hello LINE BOT!(POST)');
+        console.log('Connecting Check!');
+        return; 
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+app.listen(PORT);
+console.log("Server running at port " + PORT);
